97ca7ca0c0aabe6fef7e8ecf9bca9bbbcca58b44
Vitali Tatarintev <vitali.tatarintev@wimdu.com>
2016-09-30 15:55:59 +0200

    Calculate the tip

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,14 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Instance Properties & Outlets
+    let tipPercentage = 0.20
+    @IBOutlet weak var tipTotalLabel: UILabel!
+    @IBOutlet weak var tipAmountTextField: UITextField!
+
+    // Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        tipTotalLabel.text = "Hello"
+        tipAmountTextField.text = "0.00"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // IBActions
+    @IBAction func calculateTipPressed(_ sender: UIButton) {
+        guard let billTotal = tipAmountTextField.text else {
+            return
+        }
+
+        guard let safeBillTotal = Double(billTotal) else {
+            return
+        }
+
+        let tipAmount = safeBillTotal * tipPercentage
+        tipTotalLabel.text = "Tip: \(tipAmount)"
     }
 
 
